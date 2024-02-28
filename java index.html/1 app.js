02b380c203eadcfa9fb59646c53aaee1f8014aaf
AAMIRAAJ <aamir.abdulmajeed@gmail.com>
2024-02-29 03:05:37 +0500

    java js class 1

---
+++ java index.html/1 app.js
@@ -0,0 +1,52 @@
+//alert("hello world");
+
+//var a = 5;
+//var b = 10;
+//var c = a+b;
+//alert (c)
+
+//var a = 5;
+//var b = 10;
+//var c = a+b;
+//console.log(c);
+
+//var x = 5;
+//var y = "aamir";
+//var z = y+x;
+//alert(z);
+
+//var aamir1 = 20;
+
+//var 1aamir = 20;
+
+//var $aamir = 20;
+
+// var f = 20;
+// var g = "10";
+// var h = f+g;
+// alert(h);
+
+// var f = 20;
+// var g = "2";
+// var h = f/g;
+// alert(h);
+
+// var a = 3;
+// var b = 5;
+// var c = 7;
+// var d = (b - a) * c;
+// alert(d);
+
+// var a = 5;
+// var b = a++;
+// alert(b);
+
+// var a = 5;
+// var b = ++a;
+// alert(b);
+
+var x = 10;
+var y = x++ + ++x - x + x++ - ++x + x;
+    // 10 + 12 - 12 + 12 - 14 + 14
+    // 22
+var z = alert(y);        
